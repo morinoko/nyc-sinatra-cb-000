06c0135bf196fcc39e01e1c3df9db4ffa8882210
Felice Forby <feliceforby@gmail.com>
2018-08-01 22:55:08 +0000

    basic controller setup for figures and landmarks

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
 
+  post '/figures' do
+
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+
+    erb :'figures/edit'
+  end
+
+  post '/figures/:id' do
+
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,28 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
 
+  post '/landmarks' do
+
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+
+  end
 end
