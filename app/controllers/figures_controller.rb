979933375ff5843562f86af26a270d5612f17c12
Felice Forby <feliceforby@gmail.com>
2018-08-02 22:52:26 +0000

    enable creating and editing figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,10 +5,27 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    @figure = Figure.new(params[:figure])
 
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+
+    @figure.save
+
+    redirect to "figures/#{@figure.id}"
   end
 
   get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+
     erb :'figures/new'
   end
 
@@ -20,11 +37,28 @@ class FiguresController < ApplicationController
 
   get '/figures/:id/edit' do
     @figure = Figure.find_by_id(params[:id])
-    
+    @landmarks = Landmark.all
+    @titles = Title.all
+
     erb :'figures/edit'
   end
 
   post '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
 
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+      @figure.save
+    end
+
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+      @figure.save
+    end
+
+    redirect to "figures/#{@figure.id}"
   end
 end
